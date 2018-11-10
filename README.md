947ef22d4eaeb2d1ae669ab0ee04d53cfc98f95a
Nali M <nmavilmada@gmail.com>
2018-11-10 00:58:45 -0500

    second

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-# Word_Guess_Game
-# This is a word guess game where the user tries to guess the word 
-# The computer will give the user an x number of guesses and the user needs to guess the word before all the chances are used.
-# The theme of the game is based on world Geogrpahy.
+# Psychic Game
+# The purpose of this game is to guess the letter of the alphabet the Computer is thinkin of
+# The computer will give the user an x number of guesses and the user needs to guess the letter before numer of guesses are used up
+# If the user guesses the correct letter, then it will add do a count up in the "win section"#
+# if the user guesses incorrectly then the number of guesses will count down.
+# the "lose section" will count up
+# once the number of guesses run out then the game will be over.
+# the purpose of the game is to have the most number of wins before user guesses run out.
+
---
+++ assets/javascript/game.js
@@ -0,0 +1,65 @@
+
+var options=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var wins=0;
+var losses=0;
+var guessesLeft=10;
+var guessedLetters=[];
+
+var directionsText = document.getElementById("directions-text");
+var userChoiceText = document.getElementById("userchoice-text");
+var computerChoiceText = document.getElementById("computerchoice-text");
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+var guessesLeftText = document.getElementById("guessesLeft-text");
+var alreadyGuessedText = document.getElementById("alreadyGuessed-text");
+
+
+
+
+        
+    
+document.onkeyup=function(event){
+
+var userGuess=String.fromCharCode(event.keyCode).toLowerCase();
+console.log(userGuess)   
+
+computerGuess=options[Math.floor(Math.random()*options.length)]
+console.log(computerGuess)
+
+for (var i=0;i<options.length;i++) {
+    if(userGuess=='a'||userGuess=='b'||userGuess=='c'||userGuess=='d'||userGuess=='e'||userGuess=='f'||userGuess=='g'||userGuess=='h'||userGuess=='i'||
+         userGuess=='j'||userGuess=='k'||userGuess=='l'||userGuess=='m'||userGuess=='n'||userGuess=='o'||userGuess=='p'||userGuess=='q'||userGuess=='r'||
+         userGuess=='s'||userGuess=='t'||userGuess=='u'|| userGuess=='v'||userGuess=='w'||userGuess=='x'||userGuess=='y'||userGuess=='z'){
+
+        if(userGuess === computerGuess[i]){
+             wins++;  
+        } else if (userGuess[i]!==computerGuess[i]){
+            losses++;
+            guessesLeft--;
+            guessedLetters.push(userGuess[i]);
+            }
+        
+        if(losses>10){
+            alert("GAME OVER")
+            losses=0;
+            wins=0;
+            guessesLeft=10;
+            guessedLetters=[];
+            userGuess=[];
+            computerGuess=[];
+        }
+        
+    }
+      
+      
+        
+    directionsText.textContent= "";
+    userChoiceText.textContent = "You chose: " + userGuess;
+    computerChoiceText.textContent = "The computer chose: " + computerGuess;
+    winsText.textContent = "wins: " + wins;
+    lossesText.textContent = "losses: " + losses;
+    guessesLeftText.textContent = "guesses left: " + guessesLeft;
+    alreadyGuessedText.textContent="Letters guessed: " + guessedLetters;
+  }
+
+}
